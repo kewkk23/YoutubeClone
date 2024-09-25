85f995a99754a7d4816ea57496f7bbc2d90c6730
kewkk23 <kew.kul2137@gmail.com>
2024-09-25 16:24:06 +0200

    Initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Nowe', icon: <HomeIcon />, },
+  { name: 'Kewnii', icon: <CodeIcon />, },
+  { name: 'Programowanie', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Muzyka', icon: <MusicNoteIcon /> },
+  { name: 'Edukacja', icon: <SchoolIcon />, },
+  { name: 'Podkasty', icon: <GraphicEqIcon />, },
+  { name: 'Filmy', icon: <OndemandVideoIcon />, },
+  { name: 'Gry', icon: <SportsEsportsIcon />, },
+  { name: 'Na żywo', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Moda', icon: <CheckroomIcon />, },
+  { name: 'Uroda', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Komedie', icon: <TheaterComedyIcon />, },
+  { name: 'Gimnastyka', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCG_GRyvVE626KyrrtRDWUUQ';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'Kewniii';
+export const demoVideoTitle = 'Test';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
